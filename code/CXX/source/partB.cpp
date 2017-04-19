91af62b6d12ee7b939cae3ae9c11b5ff1106da71
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-04-18 22:53:28 -0600

    dbg

--- code/CXX/source/partB.cpp
+++ code/CXX/source/partB.cpp
@@ -218,6 +218,7 @@ arma::vec Nu_update(unsigned int m,
 
 	arma::mat test = (Ybar *  Gamma.row(m).t());
 	std::cout << test.col(0) << std::endl;
+	std::cout << Ybar.col(0) << std::endl;
 
 	return (Ybar *  Gamma.row(m).t()) / arma::sum(Gamma.row(m));
 
